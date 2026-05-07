0de546a2fc540c9dc9db671d42f4d51f5eaa7a98
anl084 <anl084@ucsd.edu>
2026-05-07 15:49:22 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // voice generation within the dropdown
   const synth = window.speechSynthesis;
-  const voiceSelect = document.getElementById("voice-select")
+  const voiceSelect = document.getElementById("voice-select");
   let voices = [];
   function populateVoiceList() {
     voices = synth.getVoices();
@@ -25,11 +25,6 @@ function init() {
   }
   populateVoiceList();
 
-  // Some browsers load voices asynchronously
-  // if (speechSynthesis.onvoiceschanged !== undefined) {
-  //   speechSynthesis.onvoiceschanged = populateVoiceList;
-  // }
-
   //Press to talk function
   const clickToPlay = document.querySelector("button");
   clickToPlay.addEventListener("click", ()=>{
@@ -40,7 +35,7 @@ function init() {
 
     //Finding the index the new value is from, since it would mtach the voices list given
     const voices = speechSynthesis.getVoices();
-    const index = document.querySelector("select[name='voice']").value; 
+    const index = document.getElementById("voice-select").value; 
     messageObject.voice = voices[index];
 
     //creating the before and after changes of when the voice speaks
