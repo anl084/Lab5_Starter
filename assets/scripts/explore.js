2aca9c3383a666e101022b4141814635606e3706
anl084 <anl084@ucsd.edu>
2026-05-07 20:44:25 -0700

    fix through using the api code and reading a bit more

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,57 +3,56 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // voice generation within the dropdown
   const synth = window.speechSynthesis;
-  const voiceSelect = document.getElementById("voice-select");
+  const inputButton = document.querySelector("button");
+  const inputTxt = document.querySelector("textarea");
+  const voiceSelect = document.querySelector("select");
+  const imageSelect = document.querySelector("img");
+  
   let voices = [];
+  
   function populateVoiceList() {
     voices = synth.getVoices();
-    let index  = 0;
+  
     for (const voice of voices) {
       const option = document.createElement("option");
       option.textContent = `${voice.name} (${voice.lang})`;
-
+  
       if (voice.default) {
         option.textContent += " — DEFAULT";
       }
-
+  
       option.setAttribute("data-lang", voice.lang);
       option.setAttribute("data-name", voice.name);
-      option.value = index;
-      index++;
       voiceSelect.appendChild(option);
     }
   }
-
+  
   populateVoiceList();
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
+  
+  inputButton.onclick = (event) => {
+    event.preventDefault();
+  
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (const voice of voices) {
+      if (voice.name === selectedOption) {
+        utterThis.voice = voice;
+      }
+    }
 
-  // Press to talk function
-  const clickToPlay = document.querySelector("button");
-  clickToPlay.addEventListener("click", ()=>{
-    const textToSpeak = document.querySelector("select[name='text-to-speak']");
-    const smilingImage = document.querySelector("img");
-
-    const messageObject = new SpeechSynthesisUtterance(textToSpeak.value);
-
-    //Finding the index the new value is from, since it would mtach the voices list given
-    let index1 = voiceSelect.value; 
-    messageObject.voice = voices[index1];
-
-    //creating the before and after changes of when the voice speaks
-    messageObject.onstart = () => {
-      smilingImage.src = "./assets/images/smiling-open.png";
+    utterThis.onstart = (event) => {
+      imageSelect.src = "assets/images/smiling-open.png";
     };
 
-    messageObject.onend = () => {
-      smilingImage.src = "./assets/images/smiling.png";
+    utterThis.onend = (event) => {
+      imageSelect.src = "assets/images/smiling.png";
     };
-
-    //Speaking it
-    speechSynthesis.speak(messageObject);
-
-  })
+    
+    synth.speak(utterThis);
+  };
 }
